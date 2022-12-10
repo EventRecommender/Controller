c4e1a4ce177d931711f1d72348e5e0ada33088c8
Villiamfj <villiamfj@hotmail.dk>
2022-12-10 15:21:07 +0100

    Update runALl.ps1

--- runALl.ps1
+++ runALl.ps1
@@ -9,14 +9,7 @@ git clone -b dev https://$token@github.com/EventRecommender/ActivityService ./ac
 git clone -b dev https://$token@github.com/EventRecommender/UserManagerService ./usermanager
 git clone -b Dev https://$token@github.com/EventRecommender/RecommenderService ./recommender
 
-# start each service
-docker-compose -f ./activity/docker-compose.yml up -d --build
-docker-compose -f ./usermanager/UserManager/docker-compose.yml up -d --build  # might change <-
-docker-compose -f ./recommender/docker-compose.yml up -d --build
-
-# building
-docker build . --tag controller
-docker run -d --name controller-service -p 80:80 controller
+docker-compose -f docker-composeJoined.yml up --build
 
 # stop
 Write-host "script ended"
