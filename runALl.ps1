53248b183a9d1faa95ec8645050083b5c02598b5
Villiam <Vjacob19@student.aau.dk>
2022-12-20 21:56:54 +0100

    Last commit no token needed in runAll.ps1

--- runALl.ps1
+++ runALl.ps1
@@ -1,14 +1,11 @@
-# read access token
-$token = Get-content ./token.txt
-
 # cloning frontend
-git clone https://$token@github.com/EventRecommender/EventsRecommenderFrontend.git ./Controller/ClientApp
+git clone https://github.com/EventRecommender/EventsRecommenderFrontend.git ./Controller/ClientApp
 
 # clone services
-git clone https://$token@github.com/EventRecommender/ActivityService ./activity
-git clone https://$token@github.com/EventRecommender/UserManagerService ./usermanager
-git clone https://$token@github.com/EventRecommender/RecommenderService ./recommender
-git clone https://$token@github.com/EventRecommender/eventScraper.git ./scraper
+git clone https://github.com/EventRecommender/ActivityService ./activity
+git clone https://github.com/EventRecommender/UserManagerService ./usermanager
+git clone https://github.com/EventRecommender/RecommenderService ./recommender
+git clone https://github.com/EventRecommender/eventScraper.git ./scraper
 
 docker-compose -f docker-composeSingle.yml up --build
 
