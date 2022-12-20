95660dc5eb29edc43d6d2f86d94793c0f67a744e
Villiam <Vjacob19@student.aau.dk>
2022-12-20 21:23:04 +0100

    Changes for running multiple replicas + removed debug script

--- runControllerOnly.ps1
+++
@@ -1,8 +0,0 @@
-$token = Get-content ./token.txt
-
-# cloning frontend
-git clone -b dev https://$token@github.com/EventRecommender/EventsRecommenderFrontend.git ./Controller/ClientApp
-
-docker build . --tag controllerdebug
-
-docker run -p 80:80 controllerdebug
